9e4c873f4f7678517d958db01f4c7ca21e338559
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2018-09-16 16:39:13 +0200

    [init] Initialize project

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+console.log(__dirname);
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+let players = {};
+
+io.on('connection', socket => {
+	socket.on('NEW_PLAYER', (data) => {
+		players[socket.id] = {
+			username: data.username,
+			colors: [ 'red', 'green', 'blue'],
+		};
+		console.log(players);
+	});
+});
+
+setInterval( () => {
+	io.sockets.emit('message', 'hi');
+}, 20000 );
+
+server.listen(5000, () => console.log('Starting server on port 5000') );
+
